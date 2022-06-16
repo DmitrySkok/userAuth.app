9b111d937f3754987d2865c193104b1a8586bffd
Dmitry Skok <dimas.skok95@gmail.com>
2022-06-16 13:34:55 +0200

    Add new pages and routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  res.render('logged', {user: req.user._json.name, image: req.user._json.picture});
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', (req, res) => {
+  res.render('settings');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.render('logout');
 });
 
 
